345c2d322c43e21fee2f7fcf43ef5984c2752c3b
PutriDiantariManurung <411201902871@mhs.dinus.ac.id>
2021-08-14 17:13:36 +0700

    initial commit

---
+++ Hello.R
@@ -0,0 +1,19 @@
+33 + 60
+60 - 33
+5 * 50
+50 / 5
+nilai_ujian <- 90
+nilai_ujian2 <- 95
+nilai_ujian + nilai_ujian2
+(nilai_ujian + nilai_ujian2 ) / 2
+nama <- "Putri Diantari Manurung"
+nama
+paste("Selamat Datang", nama)
+dt_nilai = c(95, 90, 85, 80)
+dt_nilai
+dt_nilai [1]
+dt_nilai [2]
+dt_nilai [3]
+dt_nilai [4]
+dt_nama = c("Manis", "Catty", "Ciby", "Hero")
+data.frame(dt_nama, dt_nilai)
